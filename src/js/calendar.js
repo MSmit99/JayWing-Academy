40abe23ee6126675e47c52017d062aea59bca031
muzzy3k <mmi15@protonmail.com>
2024-10-21 20:08:26 -0400

    created sql script to intialize database, added fullcalendar library

---
+++ src/js/calendar.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+}) ();
