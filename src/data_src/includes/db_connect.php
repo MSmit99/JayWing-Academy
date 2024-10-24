ad8c5f258785edb6dd0afdc92dbaf6064f416113
Joey Wagner <joeyawagner22@gmail.com>
2024-10-24 10:06:14 -0400

    Potentially breaking everything

--- src/data_src/includes/db_connect.php
+++ src/data_src/includes/db_connect.php
@@ -3,7 +3,7 @@ require_once "db_config.php";
 
 
 //Create database connection
-$connection = new mysqli($host, $dbUsername, $dbPassword, $database);
+$connection = new mysqli($host, $dbUsername, $dbPassword, $database, 3307);
 
 //Check if the connection was successful
 if ($connection->connect_error) {
