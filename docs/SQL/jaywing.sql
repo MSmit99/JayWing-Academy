5ea8b247a3f7625a7d9cda6a282fc7d2990d591a
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-11-21 09:56:20 -0500

    SQL/ERD Comments Update

--- docs/SQL/jaywing.sql
+++ docs/SQL/jaywing.sql
@@ -7,6 +7,11 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- Changes for 2.1 version:
+  -- Added back Chat_Participant table
+  -- Added 2 more attributes to User table
+  -- Connected messages back to User table
+
 -- -----------------------------------------------------
 -- Schema jaywing
 -- -----------------------------------------------------
