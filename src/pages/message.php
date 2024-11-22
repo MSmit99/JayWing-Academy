41cfbeec9d550a0c95327be3e7394fcd01084f8d
muzzy3k <mmi15@protonmail.com>
2024-11-21 21:12:51 -0500

    adjust footer size, fix the css path in message.php to properly load css

--- src/pages/message.php
+++ src/pages/message.php
@@ -78,7 +78,7 @@ $currentChat = isset($_GET['chat_id']) ? $_GET['chat_id'] : null;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- custom css -->
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body class="bg-gray-100 min-h-screen">
     <header>
